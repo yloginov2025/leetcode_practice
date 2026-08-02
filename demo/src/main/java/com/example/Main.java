b6e5be596479454ca664c87e6995f6854e5b8321
yloginov2025 <yloginov2011@gmail.com>
2026-08-02 11:00:33 -0400

    boiler plate

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ demo/src/test/java/MainTets.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.example.Main;
+
+public class MainTets {
+
+    @Test
+    void testMultiply() {
+        Main main = new Main();
+        Assertions.assertEquals(6, main.multiply(2, 3), "2 * 3 should equal 6");
+    }
+
+    @Test
+    void testDivide() {
+        Main main = new Main();
+        Assertions.assertEquals(2, main.divide(6, 3), "6 / 3 should equal 2");
+    }
+
+}
